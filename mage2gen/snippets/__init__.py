1e949a57100ec3d941298c80e53158ea4a2c4973
Lewis Voncken <lewisvoncken@happyhorizon.com>
2024-12-03 18:24:53 +0100

    [BUGFIX] fix invalid graphql snippet and removed deprecated router locator

--- mage2gen/snippets/__init__.py
+++ mage2gen/snippets/__init__.py
@@ -24,7 +24,6 @@ from .configurationtype import ConfigurationTypeSnippet
 from .block import BlockSnippet
 from .helper import HelperSnippet
 from .graphqlendpoint import GraphQlEndpointSnippet
-from .graphqlroutelocator import GraphQlRouteLocatorSnippet
 from .preference import PreferenceSnippet
 from .viewmodel import ViewModelSnippet
 from .companyattribute import CompanyAttributeSnippet
